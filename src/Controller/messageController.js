61392636870da1d41188c7210f4d92d437e53743
butrico31 <butrico0@gmail.com>
2025-07-16 10:00:56 -0300

    fix: colocando await na função que precisava

--- src/Controller/messageController.js
+++ src/Controller/messageController.js
@@ -4,7 +4,7 @@ import { sendFile } from "../Service/messageService.js"
 export const file = async (req, res) => {
 
     try {
-        sendFile(req);  
+        await sendFile(req);  
         res.status(200).send('File sent successfully');  
     } catch (error) {
         res.status(500).send(error.message);
